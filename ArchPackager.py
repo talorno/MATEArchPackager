327e4542347bd1a585ca93a5d20f17cb3d0b51e4
Giovanni Talorno <giovanni.talorno@gmail.com>
2012-11-28 17:32:57 +0100

    rivista l'architettura di tutto il marchingegno

--- ArchPackager.py
+++ ArchPackager.py
@@ -3,6 +3,23 @@ import datetime
 import time
 import re
 
+
+class package:
+	name = "xxx"
+	filename = "xxx"
+	version = "0.0.0"
+	date_published = "0"
+
+	def populate(self,name,filename,version,date_pub):
+		self.name=name
+		self.filename=filename
+		self.version=version
+		self.date_pub=date_pub
+
+	def getName(self):
+		return self.name
+
+
 now = datetime.datetime.now()
 localtime = time.localtime(time.time())
 
@@ -15,37 +32,50 @@ feed[ "items" ]
 entries = []
 entries.extend( feed[ "items" ] )
 
-toCompile = []
+
+toCompile = [[package]]
+
 
 
 # suffix = "-1.5.0.tar.xz"
-
 
 
 # enqueue aggiunge un pacchetto alla coda di compilazione
 def enqueue(nomepacchetto,nomefile):
 	#blabla
 	# print nomepacchetto,nomefile
+
+	# p = package()
+
+
+	# toCompile.append(nomepacchetto,nomefile);
 	return 1 
 
 
 # nameConvert ricava il nome del pacchetto dal nome del file
-def nameConvert(nomeFile):
+def createPackage(entry):
 
 	# converted=nomeFile.strip(suffix)
-	converted = nomeFile[:-13]
-	converted = converted.title()
-	converted = converted.replace('-',' ')
-	print converted
-	#ritorna nome
-	return converted
+	name = entry.title[:-13]
+	name = name.title()
+	name = name.replace('-',' ')
+
+	version = entry.title[13:]
+
+	p=package()
+
+	p.populate(p,name,entry.title,entry.published_parsed)
+	
+	return p
+
 
 
 for i in entries:
-	# print i["title"], i["published_parsed"][0:15]
 	if i["published_parsed"] < localtime:
-		enqueue(nameConvert(i["title"]),i["title"])
+		toCompile.append(createPackage(i))
+		# createPackage(i)
+		
 
-
-
+for p in toCompile:
+	print p
 
