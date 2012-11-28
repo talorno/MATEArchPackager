6916b69f4aa8ccc749513e5e77c38bc3e0dc9aec
Giovanni Talorno <giovanni.talorno@gmail.com>
2012-11-28 03:19:43 +0100

    updated

--- ArchPackager.py
+++ ArchPackager.py
@@ -1,6 +1,7 @@
 import feedparser
 import datetime
 import time
+import re
 
 now = datetime.datetime.now()
 localtime = time.localtime(time.time())
@@ -17,25 +18,34 @@ entries.extend( feed[ "items" ] )
 toCompile = []
 
 
+# suffix = "-1.5.0.tar.xz"
+
+
+
 # enqueue aggiunge un pacchetto alla coda di compilazione
 def enqueue(nomepacchetto,nomefile):
 	#blabla
-	print nomepacchetto,nomefile
+	# print nomepacchetto,nomefile
 	return 1 
 
 
 # nameConvert ricava il nome del pacchetto dal nome del file
-def nameConvert(nomefile):
-	#ricava nome pacchetto da file
-	#ritorna nome
-	return nomefile
+def nameConvert(nomeFile):
 
+	# converted=nomeFile.strip(suffix)
+	converted = nomeFile[:-13]
+	converted = converted.title()
+	converted = converted.replace('-',' ')
+	print converted
+	#ritorna nome
+	return converted
 
 
 for i in entries:
 	# print i["title"], i["published_parsed"][0:15]
 	if i["published_parsed"] < localtime:
 		enqueue(nameConvert(i["title"]),i["title"])
+
 
 
 
